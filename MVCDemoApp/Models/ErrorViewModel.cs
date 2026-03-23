bbef4a6e4b4185af45a291bd58db2719eedad249
Gary Turner <garyturna@hotmail.com>
2026-03-24 10:00:17 +1300

    Add project files.

---
+++ MVCDemoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCDemoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
